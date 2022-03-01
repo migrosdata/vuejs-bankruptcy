5022a2655c315a3a79506565664cf96c1234068e
migrosdata <antoine.induni@gmail.com>
2022-03-01 17:05:44 +0100

    Add error component

---
+++ src/components/Error.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="display-2 font-weight-bold mb-3">Error</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" md="3">
+        <v-btn @click="restart">
+          Nouvelle prédiction
+          <v-icon large>mdi-piggy-bank</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Error",
+  methods: {
+    restart: function () {
+      this.$emit("restart");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
