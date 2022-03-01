038f8a365ae4dbe5479b902b7dd71bf718a64ed1
migrosdata <antoine.induni@gmail.com>
2022-03-01 17:06:15 +0100

    Update result layout

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,22 +1,26 @@
 <template>
   <v-container>
-                <v-col cols="12" md="3">
-                <h1 class="display-4">{{ this.reponse.result.probaPercentile}} % </h1>
-            </v-col>
-        </v-row>
-    <div>
-      {{ this.reponse.result.prediction }}
-    </div>
-        <v-row>
-            <v-col>
-                <v-btn
-                    @click="restart"
-                >
-                    Nouvelle prédiction
-                    <v-icon large>mdi-bicycle</v-icon>            
-                </v-btn>
-            </v-col>
-        </v-row>         
+    <v-row align="center" justify="center">
+      <v-col cols="12" md="3">
+        <div v-if="this.reponse.result.prediction == 1">
+          <v-icon x-large>mdi-bank-check</v-icon>
+        </div>
+        <div v-else>
+          <v-icon x-large>mdi-bank-off-outline</v-icon>
+        </div>
+      </v-col>
+      <v-col cols="12" md="3">
+        <h1 class="display-4">{{ this.reponse.result.probaPercentile }} %</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+       <v-col cols="12" md="3" >
+        <v-btn @click="restart">
+          Nouvelle prédiction
+          <v-icon large>mdi-piggy-bank</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
