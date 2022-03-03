c90f55a94a3fb4465f136ba4af5d53f75bef3d21
migrosdata <antoine.induni@gmail.com>
2022-03-03 08:12:44 +0100

    Add error mgmt

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,22 +5,46 @@
         <v-col cols="12" md="4">
           <v-text-field
             prepend-icon="mdi-currency-usd"
-            v-model.number="form.Asset"
+            v-model.number="form.ASSETS"
             label="Asset"
             type="number"
             min="0"
             step="1"
             required
-            :rules="[v => !!v || 'Asset is required']"
-          ></v-text-field>          
+            :rules="[(v) => !!v || 'Asset is required']"
+          ></v-text-field>
         </v-col>
         <v-col cols="12" md="4">
           <v-text-field
             prepend-icon="mdi-currency-usd"
-            v-model.number="form.CurAss"
+            v-model.number="form.CFFO"
+            label="Cash flow from operations"
+            required
+            :rules="[(v) => !!v || 'Cash flow from operations is required']"
+            type="number"
+            min="0"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            v-model.number="form.CURASS"
             label="Current Asset"
             required
-            :rules="[v => !!v || 'Current Asset is required']"
+            :rules="[(v) => !!v || 'Current Asset is required']"
+            type="number"
+            min="0"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>        
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            v-model.number="form.CURDEBTS"
+            label="Current Debts"
+            required
+            :rules="[(v) => !!v || 'Current Debts is required']"
             type="number"
             min="0"
           ></v-text-field>
@@ -28,65 +52,138 @@
         <v-col cols="12" md="4">
           <v-text-field
             prepend-icon="mdi-currency-usd"
-            v-model.number="form.Debts"
+            v-model.number="form.DEBTS"
             label="Debts"
             required
-            :rules="[v => !!v || 'Debts is required']"
+            :rules="[(v) => !!v || 'Debt is required']"
             type="number"
             min="0"
           ></v-text-field>
-        </v-col>  
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            v-model.number="form.INC"
+            label="Income"
+            type="number"
+            min="0"
+            step="1"
+            required
+            :rules="[(v) => !!v || 'Income is required']"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>        
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            v-model.number="form.INCDEPT"
+            label="Income plus depreciation"
+            required
+            :rules="[(v) => !!v || 'Income plus depreciation is required']"
+            type="number"
+            min="0"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            v-model.number="form.SALES"
+            label="Sales"
+            required
+            :rules="[(v) => !!v || 'Sales is required']"
+            type="number"
+            min="0"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            v-model.number="form.WCFO"
+            label="Working capital from operation"
+            required
+            :rules="[
+              (v) => !!v || 'Working capital from operation is required',
+            ]"
+            type="number"
+            min="0"
+          ></v-text-field>
+        </v-col>
       </v-row>
       <v-row>
         <v-col cols="12" md="4">
           <v-text-field
             prepend-icon="mdi-currency-usd"
-            v-model.number="form.IncDept"
-            label="IncDept"
+            label="ASSETS/DEBTS"
+            v-model.number="ASSETS_DEBTS"
+            value="4"
             type="number"
-            min="0"
-            step="1"
-            required
-            :rules="[v => !!v || 'IncDept is required']"
-          ></v-text-field>          
-        </v-col>
-        <v-col cols="12" md="4">
-          <v-text-field
-            prepend-icon="mdi-currency-usd"
-            v-model.number="form.Inc"
-            label="Inc"
-            required
-            :rules="[v => !!v || 'Inc is required']"
-            type="number"
-            min="0"
+            disabled
           ></v-text-field>
         </v-col>
         <v-col cols="12" md="4">
           <v-text-field
             prepend-icon="mdi-currency-usd"
-            v-model.number="form.Wcfo"
-            label="Wcfo"
-            required
-            :rules="[v => !!v || 'Wcfo is required']"
+            :value="form.CURASS / form.CURDEBTS"
+            label="CURASS/CURDEBTS"
             type="number"
-            min="0"
+            disabled
           ></v-text-field>
-        </v-col>  
-      </v-row>      
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            :value="form.INCDEPT / form.DEBTS"
+            label="INCDEPT/DEBTS"
+            type="number"
+            disabled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            :value="form.WCFO / form.DEBTS"
+            label="WCFO/DEBTS"
+            type="number"
+            disabled
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            :value="form.INC / form.DEBTS"
+            label="INC/DEBTS"
+            type="number"
+            disabled
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            prepend-icon="mdi-currency-usd"
+            :value="form.INC / form.ASSETS"
+            label="INC/ASSETS"
+            type="number"
+            disabled
+          ></v-text-field>
+        </v-col>
+      </v-row>
       <v-row>
         <v-col cols="12" md="6">
-          <v-btn 
-            color="success" 
-            class="mr-4" 
+          <v-btn
+            color="success"
+            class="mr-4"
             @click="validate"
-            :disabled="!isValid">
-                Soumettre
+            :disabled="!isValid"
+          >
+            Soumettre
           </v-btn>
         </v-col>
         <v-col cols="12" md="6">
           <v-btn color="error" class="mr-4"> Reset</v-btn>
         </v-col>
-      </v-row>      
+      </v-row>
     </v-container>
   </v-form>
 </template>
@@ -96,16 +193,23 @@ export default {
   name: "Form",
   data: () => ({
     form: {},
-    debtsRules: [
-      (v) => v > 0 || "Debts is required"
-    ],
-    isValid: false
+    features: {},
+    isValid: false,
   }),
-  props: {
+  props: {},
+  computed: {
+    ASSETS_DEBTS:  {
+      get () {
+        return this.form.ASSETS / this.form.DEBTS;
+      }
+    },
   },
   methods: {
-    validate: function () {
-      this.$emit("submit", this.form);
+    validate() {
+      this.$emit("submit", this.prepareData());
+    },
+    prepareData() {
+      return { ASSETS_DEBTS : this.ASSETS_DEBTS };
     },
   },
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/bankruptMan.png" height="200" />
-    <bankruptcy-form 
-      v-on:submit="submitHandler"
-      v-if="showForm">
+    <bankruptcy-form v-on:submit="submitHandler" v-if="showForm">
     </bankruptcy-form>
     <error v-if="showError" v-on:restart="restartHandler"></error>
-    <result v-if="showResult" :reponse="dataikuResponse" v-on:restart="restartHandler"/>
+    <result
+      v-if="showResult"
+      :reponse="dataikuResponse"
+      v-on:restart="restartHandler"
+    />
   </div>
 </template>
 
@@ -20,24 +22,33 @@ export default {
   components: {
     BankruptcyForm,
     Result,
-    Error,    
+    Error,
   },
   data: () => ({
     showError: false,
     showForm: true,
     showResult: false,
-    dataikuResponse : null,
+    dataikuResponse: null,
   }),
   mounted() {
     //TODO:loin
     console.log(process.env.VUE_APP_API);
-    console.log('yo');
+    console.log("yo");
   },
   methods: {
     submitHandler(form) {
       this.showForm = false;
       const features = `{ "features" : ${JSON.stringify(form, null, " ")} }`;
       console.log(features);
+      console.log(features.replace("_", "/"));
+      this.predictBankruptcy(features);
+    },
+    restartHandler() {
+      this.showResult = false;
+      this.dataikuResponse = null;
+      this.showForm = true;
+    },
+    predictBankruptcy(features) {
       this.axios
         .post(
           "https://dataiku.hes-so.ch:8080/public/api/v1/bi21bankruptcy/bankruptcyprediction/predict",
@@ -54,11 +65,6 @@ export default {
           this.showError = true;
           console.log(error);
         });
-    },
-    restartHandler() {
-      this.showResult = false;
-      this.dataikuResponse = null;
-      this.showForm = true;
     },
   },
 };
